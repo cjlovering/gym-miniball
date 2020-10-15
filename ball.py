8446553671c2ae2abfa6d352fb226ea621959b22
cjlovering <cjlovering@wpi.edu>
2020-10-15 18:19:29 -0400

    add quadrant location starting

--- ball.py
+++ ball.py
@@ -33,6 +33,35 @@ WIDTH = 64
 HEIGHT = 64
 CHANNELS = 3
 
+𝛑 = math.pi
+𝛕 = 2 * math.pi
+SPEED = 0.01
+INTERNAL = 100
+VARIANCE = 8
+BALL_SPEED = 1 * SPEED
+PLAYER_SPEED = 0.5 * SPEED
+VISIBLE = True
+
+DEFAULT_CONFIG = {
+    "grid": {
+        "height": 64,
+        "width": 64
+    },
+    "player": {
+        "y": 5,
+        "length": 8
+    },
+    "balls": {
+        "number": 1,
+        "quadrant": "3",
+    },
+    "platform": {
+        "number": 1,
+        "quadrant": "1",
+    }
+}
+
+
 
 def reflect_boundary_function(n_x_loc, n_y_loc, item):
     if (n_x_loc - item.radius <= 0) or (n_x_loc + item.radius >= WIDTH):
@@ -138,7 +167,6 @@ class Ball:
                         self.direction = (3 * 𝛑 - self.direction) % 𝛕
 
                 # if (p.y - p.width / 2 < n_y) or (n_y > p.y + p.width / 2):
-
                 # else:
                 #     self.direction = (3 * 𝛑 - self.direction) % 𝛕
                 at_boundary = True
@@ -146,23 +174,22 @@ class Ball:
 
         # n_x, n_y = self.movement_function(self.t, self)
         # at_boundary = self.boundary_function(n_x, n_y, self)
-        print(self.x, self.y, n_x, n_y, direction, self.direction, at_boundary)
         if not at_boundary:
             self.x = n_x
             self.y = n_y
-        else:
-            n_x, n_y = self.movement_function(self)
-            stuck = redirect_on_platform(n_x, n_y, self.radius, platforms[0])
-            # print(stuck, n_x, n_y)
-            # # if stuck:
-            # #     print("STUCK!")
-            if stuck:
-                self.x = n_x
-                self.y = n_y
-            # pass
-            pass
+        # else:
+        #     n_x, n_y = self.movement_function(self)
+        #     stuck = redirect_on_platform(n_x, n_y, self.radius, platforms[0])
+        #     # print(stuck, n_x, n_y)
+        #     # # if stuck:
+        #     # #     print("STUCK!")
+        #     if stuck:
+        #         self.x = n_x
+        #         self.y = n_y
+        #     # pass
+        #     pass
 
-        self.t += 1
+        # self.t += 1
 
 
 @dataclasses.dataclass
@@ -231,8 +258,7 @@ class PlayerPlatform:
 
 class BallEnv(core.Env):
     metadata = {"render.modes": ["human", "rgb_array"], "video.frames_per_second": 2500}
-
-    def __init__(self, items, platforms, internal_steps=100, visible=False):
+    def __init__(self, items=None, platforms=None, config=None, internal_steps=INTERNAL, visible=VISIBLE):
         self.viewer = None
         self.internal_steps = internal_steps
         self.observation_space = spaces.Box(
@@ -243,8 +269,13 @@ class BallEnv(core.Env):
         self.visible = visible
         self.seed()
 
-        self.items = items
-        self.platforms = platforms
+        self.config = config
+
+        if config is not None:
+            self.items, self.platforms = generate_items(self.config)
+        else:
+            self.items = items
+            self.platforms = platforms
 
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
@@ -254,12 +285,12 @@ class BallEnv(core.Env):
         self.viewer.close()
 
     def reset(self):
-        self.state = self.np_random.uniform(low=-0.1, high=0.1, size=(4,))
-        return self._get_ob()
+        if self.config is None:
+            assert False
+        self.items, self.platforms = generate_items(self.config)
+        return self.step(0)
 
     def step(self, a):
-        s = self.state
-
         for _ in range(self.internal_steps):
             for i in self.platforms:
                 i.step()
@@ -279,38 +310,102 @@ class BallEnv(core.Env):
         o = self.viewer.render(return_rgb_array=True)
         return o
 
+def get_random_location(partition, width, height, delta=2):
+    if partition == "1":
+        x_left, x_right = width / 2, width
+        y_bot, y_top = height * 3 / 4, height
+    elif partition == "2":
+        x_left, x_right = 0, width / 2
+        y_bot, y_top = height * 3 / 4, height
+    elif partition == "3":
+        x_left, x_right = 0, width / 2
+        y_bot, y_top = height / 2, height * 3 / 4
+    elif partition == "4":
+        x_left, x_right = width / 2, width
+        y_bot, y_top = height / 2, height * 3 / 4 
+    
+    x = np.clip(np.random.normal((x_left + x_right)/2, VARIANCE), x_left+delta, x_right-delta)
+    y = np.clip(np.random.normal((y_bot + y_top)/2, VARIANCE), y_bot+delta, y_top-delta)
+    return x, y
+
+def generate_items(config):
+    items = []
+    platforms = []
+
+    grid_config = config["grid"]
+    player_config = config["player"]
+    platform_center = np.clip(
+        np.random.normal(grid_config["width"] / 2, np.sqrt(grid_config["width"])),
+        player_config["length"] / 2,
+        grid_config["width"] - player_config["length"] / 2
+    )
+
+    direction = np.random.choice([0, 𝛑])    
+    platforms.append(
+        PlayerPlatform(platform_center, player_config["y"], SPEED,  direction, length=player_config["length"])
+    )
+
+    balls_config = config["balls"]
+    for _ in range(balls_config["number"]):
+        direction = np.random.uniform()
+        x, y = get_random_location(balls_config["quadrant"], grid_config["width"], grid_config["height"])
+        items.append(Ball(x, y, BALL_SPEED, direction * 𝛕))
+
+
+    platform_config = config["platform"]
+    for _ in range(balls_config["number"]):
+        x, y = get_random_location(platform_config["quadrant"], grid_config["width"], grid_config["height"])
+        platforms.append(Platform(x, y, 0, 0))
+
+    # env = BallEnv(items, platforms, INTERNAL, VISIBLE)
+    return items, platforms
+
 
 if __name__ == "__main__":
-    import pyglet
-    import time
+    
+    # start = time.time()
+    env = BallEnv(config=DEFAULT_CONFIG)
+    for _ in range(10):
+        env.reset()
+        for _ in range(100):
+            env.step(0)
 
-    SPEED = 0.01
-    env = BallEnv(
-        [
-            Ball(10, 10, SPEED, 0.25 * 𝛑),
-            Ball(10, 10, SPEED, 0.65 * 𝛑),
-            Ball(25, 40, SPEED, 0.50 * 𝛑),
-            Ball(10, 40, SPEED, 1.65 * 𝛑),
-            Ball(10, 50, SPEED, 5.65 * 𝛑),
-            Ball(10, 10, SPEED, 0.22 * 𝛑),
-            Ball(10, 10, SPEED, 0.54 * 𝛑),
-            Ball(25, 40, SPEED, 3.64 * 𝛑),
-            Ball(10, 40, SPEED, 4.65 * 𝛑),
-            Ball(10, 50, SPEED, 5.65 * 𝛑),
-        ],
-        [
-            PlayerPlatform(32, 1, SPEED, 𝛕, length=8),
-            Platform(25, 50, 0, 0),
-            Platform(45, 25, 0, 0),
-        ],
-        visible=True,
-        internal
-    )
-    STEPS = 1000
-    start = time.time()
-    for _ in range(STEPS):
-        env.step("n/a")
-    end = time.time()
-    print(STEPS / (end - start))
+    # end = time.time()
+    # print(1000 / (end - start))
     env.close()
+
+
+
+    # import pyglet
+    # import time
+
+    # SPEED = 0.01
+    # env = BallEnv(
+    #     [
+    #         Ball(10, 10, SPEED, 0.25 * 𝛑),
+    #         Ball(10, 10, SPEED, 0.65 * 𝛑),
+    #         Ball(25, 40, SPEED, 0.50 * 𝛑),
+    #         Ball(10, 40, SPEED, 1.65 * 𝛑),
+    #         Ball(10, 50, SPEED, 5.65 * 𝛑),
+    #         Ball(10, 10, SPEED, 0.22 * 𝛑),
+    #         Ball(10, 10, SPEED, 0.54 * 𝛑),
+    #         Ball(25, 40, SPEED, 3.64 * 𝛑),
+    #         Ball(10, 40, SPEED, 4.65 * 𝛑),
+    #         Ball(10, 50, SPEED, 5.65 * 𝛑),
+    #     ],
+    #     [
+    #         PlayerPlatform(32, 1, SPEED, 𝛕, length=8),
+    #         Platform(25, 50, 0, 0),
+    #         Platform(45, 25, 0, 0),
+    #     ],
+    #     visible=True,
+    #     internal_steps=100
+    # )
+    # STEPS = 1000
+    # start = time.time()
+    # for _ in range(STEPS):
+    #     env.step("n/a")
+    # end = time.time()
+    # print(STEPS / (end - start))
+    # env.close()
 
