12fbde3ccee6e73f166e514b67bd88a6d4b832c0
cjlovering <cjlovering@wpi.edu>
2020-10-17 06:37:01 -0400

    update game to have actions + added human control

--- ball.py
+++ ball.py
@@ -33,34 +33,25 @@ WIDTH = 64
 HEIGHT = 64
 CHANNELS = 3
 
-𝛑 = math.pi
-𝛕 = 2 * math.pi
+
 SPEED = 0.01
 INTERNAL = 100
-VARIANCE = 8
+VARIANCE = 4
 BALL_SPEED = 1 * SPEED
 PLAYER_SPEED = 0.5 * SPEED
 VISIBLE = True
 
+BOTTOM_DANGER = True
 DEFAULT_CONFIG = {
-    "grid": {
-        "height": 64,
-        "width": 64
-    },
-    "player": {
-        "y": 5,
-        "length": 8
-    },
-    "balls": {
-        "number": 1,
-        "quadrant": "3",
-    },
-    "platform": {
-        "number": 1,
-        "quadrant": "1",
-    }
+    "grid": {"height": 64, "width": 64},
+    "player": {"y": 5, "length": 8},
+    "balls": {"number": 1, "quadrant": "3",},
+    "platform": {"number": 1, "quadrant": "1",},
 }
 
+SKIP_ACTION = 0
+BOOST_LEFT_ACTION = 1
+BOOST_RIGHT_ACTION = 2
 
 
 def reflect_boundary_function(n_x_loc, n_y_loc, item):
@@ -71,6 +62,7 @@ def reflect_boundary_function(n_x_loc, n_y_loc, item):
         item.direction = (𝛕 - item.direction) % 𝛕
         return True
     return False
+
 
 def redirect_on_platform(n_x, n_y, radius, platform):
     item_x_boundary = n_x - radius, n_x + radius
@@ -108,8 +100,11 @@ def point_inside(n_x, n_y, radius, point):
     return (n_x - radius <= point[0] and point[0] <= n_x + radius) and (
         n_y - radius <= point[1] and point[1] <= n_y + radius
     )
+
+
 def intersects(a, b):
     return not (a[0] > b[1] or a[1] < b[0])
+
 
 @dataclasses.dataclass
 class Ball:
@@ -138,6 +133,8 @@ class Ball:
             intersected = redirect_on_platform(n_x, n_y, self.radius, p)
             if intersected:
                 finished = False
+
+                # First, we check if we hit a corner.
                 l, r, t, b = (
                     p.x + -p.length / 2,
                     p.x + p.length / 2,
@@ -147,6 +144,8 @@ class Ball:
                 corners = [(l, b), (l, t), (r, t), (r, b)]
                 for point in corners:
                     if point_inside(n_x, n_y, self.radius, point):
+                        # Adopted the logic for handling corners from stackexchange:
+                        # https://gamedev.stackexchange.com/questions/10911/a-ball-hits-the-corner-where-will-it-deflects
                         ball_dx = math.cos(self.direction)
                         ball_dy = math.sin(self.direction)
                         x = self.x - point[0]
@@ -158,38 +157,32 @@ class Ball:
                         at_boundary = True
                         finished = True
                         break
+
+                # If we did not hit a corner, check if we hit the top/bot or a side.
                 if not finished:
+                    # Top / bot side.
                     if (p.x - p.length / 2 <= self.x) and (
                         self.x <= p.x + p.length / 2
                     ):
                         self.direction = (𝛕 - self.direction) % 𝛕
+                    # Left / right side
                     else:
                         self.direction = (3 * 𝛑 - self.direction) % 𝛕
-
-                # if (p.y - p.width / 2 < n_y) or (n_y > p.y + p.width / 2):
-                # else:
-                #     self.direction = (3 * 𝛑 - self.direction) % 𝛕
                 at_boundary = True
                 break
 
-        # n_x, n_y = self.movement_function(self.t, self)
-        # at_boundary = self.boundary_function(n_x, n_y, self)
+        # If we're not about to hit something (and thus bounce), move.
         if not at_boundary:
             self.x = n_x
             self.y = n_y
-        # else:
-        #     n_x, n_y = self.movement_function(self)
-        #     stuck = redirect_on_platform(n_x, n_y, self.radius, platforms[0])
-        #     # print(stuck, n_x, n_y)
-        #     # # if stuck:
-        #     # #     print("STUCK!")
-        #     if stuck:
-        #         self.x = n_x
-        #         self.y = n_y
-        #     # pass
-        #     pass
+        else:
+            # If we're playing for keeps, the bottom is dangerous!
+            if BOTTOM_DANGER:
+                if n_y - self.radius <= 0:
+                    return True
 
-        # self.t += 1
+        # All is well.
+        return False
 
 
 @dataclasses.dataclass
@@ -258,7 +251,16 @@ class PlayerPlatform:
 
 class BallEnv(core.Env):
     metadata = {"render.modes": ["human", "rgb_array"], "video.frames_per_second": 2500}
-    def __init__(self, items=None, platforms=None, config=None, internal_steps=INTERNAL, visible=VISIBLE):
+
+    def __init__(
+        self,
+        items=None,
+        platforms=None,
+        player_platform=None,
+        config=DEFAULT_CONFIG,
+        internal_steps=INTERNAL,
+        visible=VISIBLE,
+    ):
         self.viewer = None
         self.internal_steps = internal_steps
         self.observation_space = spaces.Box(
@@ -272,10 +274,20 @@ class BallEnv(core.Env):
         self.config = config
 
         if config is not None:
-            self.items, self.platforms = generate_items(self.config)
+            self.items, self.platforms, self.player_platform = generate_items(
+                self.config
+            )
         else:
             self.items = items
             self.platforms = platforms
+            self.player_platform = player_platform
+
+    def render(self):
+        self.visible = True
+        if self.viewer is None:
+            self.viewer = rendering.Viewer(SCREEN_HEIGHT, SCREEN_WIDTH)
+            self.viewer.set_bounds(0, WIDTH, 0, HEIGHT)
+        self.viewer.window.set_visible(self.visible)
 
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
@@ -287,15 +299,28 @@ class BallEnv(core.Env):
     def reset(self):
         if self.config is None:
             assert False
-        self.items, self.platforms = generate_items(self.config)
+        self.items, self.platforms, self.player_platform = generate_items(self.config)
         return self.step(0)
 
-    def step(self, a):
+    def step(self, action):
+        if action == BOOST_LEFT_ACTION:
+            self.player_platform.v = 1 * SPEED
+            self.player_platform.direction = 𝛑
+            print(f"ACTION LEFT: {action}")
+        elif action == BOOST_RIGHT_ACTION:
+            self.player_platform.v = 1 * SPEED
+            self.player_platform.direction = 0
+        elif action == SKIP_ACTION:
+            print(f"ACTION SKIPPED: {action}")
+        else:
+            print(f"ACTION UNRECOGNIZED: {action}")
+
+        done = False
         for _ in range(self.internal_steps):
             for i in self.platforms:
                 i.step()
             for i in self.items:
-                i.step(self.platforms)
+                done |= i.step(self.platforms)
 
         if self.viewer is None:
             self.viewer = rendering.Viewer(SCREEN_HEIGHT, SCREEN_WIDTH)
@@ -307,8 +332,10 @@ class BallEnv(core.Env):
         for i in self.platforms:
             i.draw(self.viewer)
 
-        o = self.viewer.render(return_rgb_array=True)
-        return o
+        observation = self.viewer.render(return_rgb_array=True)
+        reward = 1 if not done else 0
+        return observation, reward, done, {}
+
 
 def get_random_location(partition, width, height, delta=2):
     if partition == "1":
@@ -322,11 +349,18 @@ def get_random_location(partition, width, height, delta=2):
         y_bot, y_top = height / 2, height * 3 / 4
     elif partition == "4":
         x_left, x_right = width / 2, width
-        y_bot, y_top = height / 2, height * 3 / 4 
-    
-    x = np.clip(np.random.normal((x_left + x_right)/2, VARIANCE), x_left+delta, x_right-delta)
-    y = np.clip(np.random.normal((y_bot + y_top)/2, VARIANCE), y_bot+delta, y_top-delta)
+        y_bot, y_top = height / 2, height * 3 / 4
+
+    x = np.clip(
+        np.random.normal((x_left + x_right) / 2, VARIANCE),
+        x_left + delta,
+        x_right - delta,
+    )
+    y = np.clip(
+        np.random.normal((y_bot + y_top) / 2, VARIANCE), y_bot + delta, y_top - delta
+    )
     return x, y
+
 
 def generate_items(config):
     items = []
@@ -337,44 +371,52 @@ def generate_items(config):
     platform_center = np.clip(
         np.random.normal(grid_config["width"] / 2, np.sqrt(grid_config["width"])),
         player_config["length"] / 2,
-        grid_config["width"] - player_config["length"] / 2
+        grid_config["width"] - player_config["length"] / 2,
     )
 
-    direction = np.random.choice([0, 𝛑])    
-    platforms.append(
-        PlayerPlatform(platform_center, player_config["y"], SPEED,  direction, length=player_config["length"])
+    direction = np.random.choice([0, 𝛑])
+    player_platform = PlayerPlatform(
+        platform_center,
+        player_config["y"],
+        SPEED,
+        direction,
+        length=player_config["length"],
     )
 
     balls_config = config["balls"]
     for _ in range(balls_config["number"]):
-        direction = np.random.uniform()
-        x, y = get_random_location(balls_config["quadrant"], grid_config["width"], grid_config["height"])
+        direction = np.random.uniform(0.15, 0.35)
+        x, y = get_random_location(
+            balls_config["quadrant"], grid_config["width"], grid_config["height"]
+        )
         items.append(Ball(x, y, BALL_SPEED, direction * 𝛕))
-
 
     platform_config = config["platform"]
     for _ in range(balls_config["number"]):
-        x, y = get_random_location(platform_config["quadrant"], grid_config["width"], grid_config["height"])
+        x, y = get_random_location(
+            platform_config["quadrant"], grid_config["width"], grid_config["height"]
+        )
         platforms.append(Platform(x, y, 0, 0))
-
+    platforms.append(player_platform)
     # env = BallEnv(items, platforms, INTERNAL, VISIBLE)
-    return items, platforms
+    return items, platforms, player_platform
 
 
 if __name__ == "__main__":
-    
-    # start = time.time()
+
     env = BallEnv(config=DEFAULT_CONFIG)
     for _ in range(10):
         env.reset()
-        for _ in range(100):
-            env.step(0)
+        i = 0
+        while i < 1000:
+            i += 1
+            observation, reward, done, info = env.step(0)
+            if done:
+                break
 
     # end = time.time()
     # print(1000 / (end - start))
     env.close()
-
-
 
     # import pyglet
     # import time
